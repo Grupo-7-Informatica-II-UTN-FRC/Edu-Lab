5d1610b9a8570e992343f135440d80fd09c4bba2
Emanuel Lopez <manu969696@gmail.com>
2021-07-07 21:16:50 -0300

    primer main vacio

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+PROYECTO: Herramienta educativa Edu-Lab
+
+Lopez Emanuel 	Leg. 80300
+Blanco Jose 	Leg. 83621
+Muñoz Leandro 	Leg. 83393
+*/
+
+#include <stdio.h>
+
+int main (void){ //Tratemos de comentar todo lo que hagamos
+
+
+	
+}
