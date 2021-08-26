27119be2f006d608dfebd460cadd45f6dc4ec035
manulopez96 <d.manu.lopez@gmail.com>
2021-08-26 20:03:57 -0300

    corrigiendoerrores

--- minilab.h
+++ minilab.h
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <string.h>
 /* 
 MODO Mini-Lab el usuario podrá configurar los estímulos de manera manual, cada estímulo enviado y cada respuesta recibida, tendrá opción de quedar documentada en un archivo que se almacenará en un archivo de texto (.txt) en el ordenador o PC. El Mini-Lab trabajará en este modo como un asistente de ensayos, facilitando las pruebas que el usuario realice en el circuito.
 */
@@ -56,14 +56,18 @@ else{
 	printf("\n\t Ingrese su usuario: ");
 	scanf("%[^\n]s", usuario);
 	printf(" --->%s<---\n\n",usuario);
-	
+	printf(" --->%s<---\n\n",datos_1.name);
+
+	while(getchar()!='\n');
+
+
 	printf("%-15s%-7s%-7s%-7s%-7s%-7s%-7s \n", "Usuario", "IN 1", "IN 2", "IN 3", "IN 4", "out 1", "out 2");
 
 	while(!feof(ptrf)){
 	
 		fread( &datos_1,sizeof(datos),1,ptrf);
-	
-		if(usuario==datos_1.name){
+//printf("\n\n --->%s<---\n\n",datos_1.name);	
+		if((strcmp(usuario,datos_1.name))==0){
 			printf("%-15s%-7d%-7d%-7d%-7d%-7d%-7d\n",datos_1.name,datos_1.inout.in_1,datos_1.inout.in_2,datos_1.inout.in_3,datos_1.inout.in_4,datos_1.inout.out_1,datos_1.inout.out_2);
 
 		}
