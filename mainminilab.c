ec519f5b9b5b4946d0374ecc26bc710b3dffa9b7
López Emanuel <65171272+manulopez96@users.noreply.github.com>
2021-11-05 23:38:58 -0300

    Add files via upload

---
+++ mainminilab.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "minilab.h"
+
+
+//uso este main para probar funciones que voy a utilizar en la biblioteca
+int main (int argc, char *argv[]){
+datos_usuario datos_1 = {0 ,"lean",{62}};
+
+strcpy(datos_1.name, argv[1]);
+//strcpy(datos_1.inout, "5";
+
+time(&datos_1.data_time);
+//printf("\n\t%s\n",ctime(&datos_1.data_time));
+
+guardarDatos(datos_1);
+leerDatos();
+
+	return 0;
+
+}
---
+++ minilab.h
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h> 
+#define nombre 15
+
+/* 
+MODO Mini-Lab el usuario podrá configurar los estímulos de manera
+manual, cada estímulo enviado y cada respuesta recibida, tendrá 
+opción de quedar documentada en un archivo que se almacenará en 
+un archivo de texto (.txt) en el ordenador o PC. El Mini-Lab 
+trabajará en este modo como un asistente de ensayos, facilitando 
+las pruebas que el usuario realice en el circuito.
+*/
+struct DATOS {     		//Estructura para guardar datos IO
+	time_t data_time;
+	char name[nombre];
+		union{
+			unsigned int io_6 : 6;
+			struct{
+				unsigned int in_1 : 1;
+				unsigned int in_2 : 1;
+				unsigned int in_3 : 1;
+				unsigned int in_4 : 1;
+				unsigned int out_1: 1;
+				unsigned int out_2: 1;
+			};
+		}inout;
+};
+
+typedef struct DATOS datos_usuario; 							//simplificacion para declarar "DATOS"
+
+void guardarDatos(datos_usuario datos_1){ 				 //guarda los datos leidos
+
+FILE *ptrf;
+
+if (( ptrf = fopen ("datos.dat","ab"))==NULL){
+
+	printf("/n/tEl archivo no pudo abrirse");
+}
+else{
+
+	fseek(ptrf,0,SEEK_END);									//posiciona el puntero al final
+	fwrite(&datos_1,sizeof(datos_usuario),1,ptrf);	//escribe datos en un archivo
+
+	fclose (ptrf);													//cierra archivo
+
+}
+}
+
+
+
+void leerDatos(){											//lee los datos guardados
+
+char usuario_1[15] ;										//usuario a buscar
+datos_usuario datos_1 = {0, "", {0}};					//estructura donde se guarda el archivo
+
+FILE *ptrf;
+
+if (( ptrf = fopen ("datos.dat","rb"))==NULL){
+
+	printf("/n/tEl archivo no existe o no pudo abrirse");
+}
+else{
+
+	printf("\n\t Ingrese su usuario: ");	
+	scanf("%[^\n]s", usuario_1);					//usuario a buscar
+	system("clear");
+//	printf(" --->%s<---\n\n",usuario);	
+//	printf(" --->%s<---\n\n",datos_1.name);
+
+	while(getchar()!='\n');		
+
+
+	printf("%-12s%-7s%-7s%-7s%-7s%-7s%-7s%-25s\n", "Usuario", "IN 1", "IN 2", "IN 3", "IN 4", "out 1", "out 2","Fecha");
+
+	while(!feof(ptrf)){
+	
+		fread( &datos_1,sizeof(datos_usuario),1,ptrf);
+//printf("\n\n --->%s<---\n\n",datos_1.name);	
+		if(((strcmp(usuario_1,datos_1.name))==0)||((strcmp(usuario_1,"todos"))==0)){
+			printf("%-12s%-7d%-7d%-7d%-7d%-7d%-7d%-25s", datos_1.name, datos_1.inout.in_1, datos_1.inout.in_2, datos_1.inout.in_3, datos_1.inout.in_4, datos_1.inout.out_1, datos_1.inout.out_2, ctime(&datos_1.data_time) );
+
+		}
+	}
+	fclose (ptrf);
+
+}
+}
+
